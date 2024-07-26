0db94280e7f3ae85170e31e22b37c4fcfd3f6863
PavlenkoDm <dmpavlenko07@gmail.com>
2024-07-26 22:57:47 +0300

    UPD: tech-skills.scenes to new logic

--- src/telegram/scenes/join-scenes/tech-skills.scenes.ts
+++ src/telegram/scenes/join-scenes/tech-skills.scenes.ts
@@ -11,7 +11,7 @@ import { Markup, Scenes } from 'telegraf';
 import { IJoinSceneState } from './join.config';
 import {
   Forbidden,
-  onSceneGateFromCommand,
+  onSceneGateWithoutEnterScene,
 } from '../helpers-scenes/scene-gate.helper';
 import { Emoji } from 'src/telegram/emoji/emoji';
 
@@ -24,11 +24,13 @@ export class TechSkillsScene extends Scenes.BaseScene<
     super('TECH_SKILLS_SCENE');
   }
 
-  @SceneEnter()
-  async onEnterTechSkillsScene(
-    @Ctx() ctx: Scenes.SceneContext<IJoinSceneState>,
+  private techSkillsStartMessageId: number;
+  private techSkillsChoiceMessageId: number;
+
+  private async techSkillsStartMarkup(
+    ctx: Scenes.SceneContext<IJoinSceneState>,
   ) {
-    await ctx.replyWithHTML(
+    const startMessage = await ctx.replyWithHTML(
       `<b>${Emoji.question} Вкажіть програми (мови програмування, фреймворки) з якими ви працюєте і які стосуються вашої технічної спеціальності:</b>
       \n<i> ( Наприклад:  AutoCAD,  MATLAB,  SolidWorks,  JavaScript,  React )</i>
       \n${Emoji.attention} Пропускайте даний пункт якщо у вас не технічна спеціальність.`,
@@ -36,17 +38,59 @@ export class TechSkillsScene extends Scenes.BaseScene<
         Markup.button.callback(`${Emoji.skip} Пропустити`, 'skip_tech_skills'),
       ]),
     );
+
+    this.techSkillsStartMessageId = startMessage.message_id;
+
+    return startMessage;
+  }
+  private async techSkillsChoiseMarkup(
+    ctx: Scenes.SceneContext<IJoinSceneState>,
+  ) {
+    this.techSkillsChoiceMessageId &&
+      (await ctx.deleteMessage(this.techSkillsChoiceMessageId));
+    const message = await ctx.replyWithHTML(
+      `<b>${Emoji.answer} Ви працюєте з такими програмами (мовами програмування, фреймворками):</b>
+      \n"<i>${ctx.session.__scenes.state.techSkills}</i>"
+      \n${Emoji.attention} ( Для зміни доданої інформації, введіть нові дані )`,
+      Markup.inlineKeyboard([
+        [
+          Markup.button.callback(
+            `${Emoji.forward} Далі`,
+            'go-forward_to_time_period',
+          ),
+        ],
+      ]),
+    );
+
+    this.techSkillsChoiceMessageId = message.message_id;
+
+    return message;
+  }
+
+  @SceneEnter()
+  async onEnterTechSkillsScene(
+    @Ctx() ctx: Scenes.SceneContext<IJoinSceneState>,
+  ) {
+    this.techSkillsStartMessageId &&
+      (await ctx.deleteMessage(this.techSkillsStartMessageId));
+    await this.techSkillsStartMarkup(ctx);
   }
 
   @On('text')
   async onTechSkills(@Ctx() ctx: Scenes.SceneContext<IJoinSceneState>) {
-    const gate = await onSceneGateFromCommand(
+    const gate = await onSceneGateWithoutEnterScene(
       ctx,
       'TECH_SKILLS_SCENE',
       Forbidden.untilJoin,
     );
     if (gate) {
-      return;
+      if (!ctx.session.__scenes.state.techSkills) {
+        await ctx.scene.enter('TECH_SKILLS_SCENE', ctx.session.__scenes.state);
+        return;
+      } else {
+        await this.techSkillsChoiseMarkup(ctx);
+        return;
+      }
     }
 
     const message = ctx.text.trim();
@@ -58,24 +102,7 @@ export class TechSkillsScene extends Scenes.BaseScene<
       ctx.session.__scenes.state.techSkills = message;
     }
 
-    ctx.replyWithHTML(
-      `<b>${Emoji.answer} Ви працюєте з такими програмами (мовами програмування, фреймворками):</b>
-      \n"<i>${ctx.session.__scenes.state.techSkills}</i>"`,
-      Markup.inlineKeyboard([
-        [
-          Markup.button.callback(
-            `${Emoji.forward} Далі`,
-            'go-forward_to_time_period',
-          ),
-        ],
-        [
-          Markup.button.callback(
-            `${Emoji.change} Змінити додану інформацію`,
-            'change_tech_skills',
-          ),
-        ],
-      ]),
-    );
+    await this.techSkillsChoiseMarkup(ctx);
   }
 
   @Action('go-forward_to_time_period')
@@ -84,14 +111,6 @@ export class TechSkillsScene extends Scenes.BaseScene<
       return;
     }
     await ctx.scene.enter('TIME_PERIOD_SCENE', ctx.session.__scenes.state);
-  }
-
-  @Action('change_tech_skills')
-  async changeTheme(@Ctx() ctx: Scenes.SceneContext<IJoinSceneState>) {
-    if (ctx.scene.current.id !== 'TECH_SKILLS_SCENE') {
-      return;
-    }
-    await ctx.scene.enter('TECH_SKILLS_SCENE', ctx.session.__scenes.state);
   }
 
   @Action('skip_tech_skills')
